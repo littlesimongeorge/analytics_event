6e7997d657d1c1ce083200b89c24d011c2e4358b
littlesimongeorge <simon.david.george@live.co.uk>
2017-07-17 22:36:28 +0100

    Create ga_event.js

---
+++ ga_event.js
@@ -0,0 +1,8 @@
+<script>
+functionfunction event(event, category, action, label) {
+    this.eventName = event;
+    this.eventCategory = category;
+    this.eventAction = action;
+    this.eventLabel = label;
+}
+</script>
