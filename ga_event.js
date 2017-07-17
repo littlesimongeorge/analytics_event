8b77c8af4af2bd0590afc8ed0122defc4c1f8908
littlesimongeorge <simon.david.george@live.co.uk>
2017-07-17 22:39:52 +0100

    Update ga_event.js

--- ga_event.js
+++ ga_event.js
@@ -1,5 +1,5 @@
 <script>
-functionfunction event(event, category, action, label) {
+function event(event, category, action, label) {
     this.eventName = event;
     this.eventCategory = category;
     this.eventAction = action;
